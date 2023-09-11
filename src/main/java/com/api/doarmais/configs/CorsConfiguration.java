48f12ad6e8fc6340f647d9ab01787e65dacfb699
renanstopa <renanstopa@outlook.com>
2023-09-10 21:24:33 -0300

    Adição da annotation de configuration

--- src/main/java/com/api/doarmais/configs/CorsConfiguration.java
+++ src/main/java/com/api/doarmais/configs/CorsConfiguration.java
@@ -1,8 +1,10 @@
 package com.api.doarmais.configs;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
