3621bfa662e21f4ac07d6aec01375cf11a0fc502
renanstopa <renanstopa@outlook.com>
2023-11-04 19:04:31 -0300

    Adição de contas pendentes; Adição de denúncia de anúncios.

---
+++ database/07-createReport.sql
@@ -0,0 +1,5 @@
+set time_zone='America/Sao_Paulo';
+
+insert into denuncia values
+(1, 4, 3, null, 4, 41, 'Esse Xbox não é o One!!', now()),
+(2, 4, 4, null, 8, 41, 'É a segunda vez que criaram esse anúncio no mês', now());
