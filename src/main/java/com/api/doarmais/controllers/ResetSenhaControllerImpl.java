55fdb4d60846a00a8425af78bdc5746abfe7e0dc
renanstopa <renanstopa@outlook.com>
2023-11-20 15:44:20 -0300

    Fix: método de verificar se token já foi utilizado na redefinição de senha

--- src/main/java/com/api/doarmais/controllers/ResetSenhaControllerImpl.java
+++ src/main/java/com/api/doarmais/controllers/ResetSenhaControllerImpl.java
@@ -68,7 +68,7 @@ public class ResetSenhaControllerImpl implements ResetSenhaController {
     if (resetSenhaService.expirou(resetSenhaModel))
       throw new ResetExpired("Seu pedido de alteração de senha já expirou");
 
-    if (!resetSenhaModel.getIdSituacao().equals(SituacaoModel.TOKEN_UTILIZADO))
+    if (resetSenhaModel.getIdSituacao().equals(SituacaoModel.TOKEN_UTILIZADO))
       throw new LinkAlreadyUsed("Esse link já foi utilizado para alterar a senha");
 
     if (!trocarSenhaRequestDto.getNovaSenha().equals(trocarSenhaRequestDto.getConfirmaSenha()))
